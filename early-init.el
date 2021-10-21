76576a7f2f1c81ab4370509b4bb184dbd8f12c23
Eugene Mah <eugenemah@gmail.com>
2021-10-21 05:43:48 -0400

    Initial commit. early-init.el

---
+++ early-init.el
@@ -0,0 +1,8 @@
+;;; early-init.el --- -*- lexical-binding:t -*-
+
+(setq gc-cons-threshold 100000000)
+(setq package-enable-at-startup nil)
+(setq site-run-file nil)
+
+(provide 'early-init)
+;;; early-init ends here
