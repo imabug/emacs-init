69ef2463674ff317367adbe2bd55c16b32bbaeae
Eugene Mah <eugenemah@gmail.com>
2021-10-20 22:28:50 -0400

    Add some more packages

--- init.el
+++ init.el
@@ -91,7 +91,8 @@
 (require 'package)
 (setq package-enable-at-startup nil)
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                         ("melpa" . "http://melpa.org/packages/")))
+                         ("melpa" . "http://melpa.org/packages/")
+                         ("nongnu" . "https://elpa.nongnu.org/")))
 (package-initialize)
 (unless package-archive-contents
   (package-refresh-contents))
@@ -181,6 +182,14 @@
                               ("l" "Lab book"
                                entry (file+olp+datetree "~/org/PhD/notes.org")
                                "* %U\n %?\n %i\n %a")))
+(with-eval-after-load 'org
+  (org-defkey org-mode-map [(meta return)] 'org-meta-return))
+(with-eval-after-load 'org-agenda
+  (require 'org-projectile)
+  (mapcar #'(lambda (file)
+              (when (file-exists-p file)
+                (push file org-agenda-files)))
+          (org-projectile-todo-files)))
 
 ;; Magit
 (use-package magit)
@@ -189,6 +198,175 @@
 (use-package projectile
   :config
   (projectile-mode t))
+
+;; Treemacs
+(use-package treemacs
+  :init
+  (with-eval-after-load 'winum
+    (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
+  :custom
+  (treemacs-collapse-dirs 3)
+  (treemacs-deferred-git-apply-delay 0.5)
+  (treemacs-display-in-side-window t)
+  (treemacs-file-event-delay 5000)
+  (treemacs-file-follow-delay 0.2)
+  (treemacs-follow-after-init t)
+  (treemacs-follow-recenter-distance 0.1)
+  (treemacs-git-command-pipe "")
+  (treemacs-goto-tag-strategy 'refetch-index)
+  (treemacs-indentation 2)
+  (treemacs-indentation-string " ")
+  (treemacs-is-never-other-window nil)
+  (treemacs-max-git-entries 5000)
+  (treemacs-no-png-images nil)
+  (treemacs-no-delete-other-windows t)
+  (treemacs-project-follow-cleanup nil)
+  (treemacs-persist-file (expand-file-name ".cache/treemacs-persist" user-emacs-directory))
+  (treemacs-recenter-after-file-follow nil)
+  (treemacs-recenter-after-tag-follow nil)
+  (treemacs-show-cursor nil)
+  (treemacs-show-hidden-files t)
+  (treemacs-silent-filewatch nil)
+  (treemacs-silent-refresh nil)
+  (treemacs-sorting 'alphabetic-desc)
+  (treemacs-space-between-root-nodes t)
+  (treemacs-tag-follow-cleanup t)
+  (treemacs-tag-follow-delay 1.5)
+  (treemacs-width 35)
+  :config
+  ;; The default width and height of the icons is 22 pixels. If you are
+  ;; using a Hi-DPI display, uncomment this to double the icon size.
+  ;;(treemacs-resize-icons 44)
+  (treemacs-follow-mode t)
+  (treemacs-filewatch-mode t)
+  (treemacs-fringe-indicator-mode t)
+  :bind
+  (("M-0"       . treemacs-select-window)
+   ("C-x t 1"   . treemacs-delete-other-windows)
+   ("C-x t t"   . treemacs)
+   ("C-x t B"   . treemacs-bookmark)
+   ("C-x t C-t" . treemacs-find-file)
+   ("C-x t M-t" . treemacs-find-tag))
+  (:map treemacs-mode-map ("C-p" . treemacs-previous-line)))
+(use-package treemacs-magit
+  :defer t
+  :after (treemacs magit))
+(use-package treemacs-projectile
+  :defer t
+  :after (treemacs projectile))
+
+;; Smartparens
+(use-package smartparens
+  :hook (prog-mode .smartparens-mode)
+  :diminish smartparens-mode)
+(show-paren-mode t)
+
+;; Flycheck
+(require 'flycheck)
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
+;; LSP Language Server Protocol
+(use-package lsp-mode
+  :bind
+  (:map lsp-mode-map ("C-c C-f" . lsp-format-buffer))
+  :config
+  (setq lsp-headerline-breadcrumb-enable t
+        lsp-prefer-flymake nil
+        lsp-keymap-prefix "C-x l"
+        gc-cons-threshold (* 100 1024 1024)
+        read-process-output-max (* 1024 1024)
+        company-idle-delay 0.0
+        company-minimum-prefix-length 1
+        ;; lock files will kill `npm start'
+        create-lockfiles nil)
+  :hook
+  ((java-mode python-mode go-mode rust-mode
+              js-mode js2-mode typescript-mode web-mode
+              c-mode c++-mode objc-mode) . lsp-deferred))
+(use-package lsp-ui
+  :after lsp-mode
+  :diminish
+  :commands lsp-ui-mode
+  :custom-face
+    (lsp-ui-doc-background ((t (:background nil))))
+    (lsp-ui-doc-header ((t (:inherit (font-lock-string-face italic)))))
+    :bind
+    (:map lsp-ui-mode-map
+          ([remap xref-find-definitions] . lsp-ui-peek-find-definitions)
+          ([remap xref-find-references] . lsp-ui-peek-find-references)
+          ("C-c u" . lsp-ui-imenu)
+          ("M-i" . lsp-ui-doc-focus-frame))
+    (:map lsp-mode-map
+          ("M-n" . forward-paragraph)
+          ("M-p" . backward-paragraph))
+    :custom
+    (lsp-ui-doc-header t)
+    (lsp-ui-doc-include-signature t)
+    (lsp-ui-doc-border (face-foreground 'default))
+    (lsp-ui-sideline-enable nil)
+    (lsp-ui-sideline-ignore-duplicate t)
+    (lsp-ui-sideline-show-code-actions nil)
+    :config
+    ;; Use lsp-ui-doc-webkit only in GUI
+    (when (display-graphic-p)
+      (setq lsp-ui-doc-use-webkit t))
+    ;; WORKAROUND Hide mode-line of the lsp-ui-imenu buffer
+    ;; https://github.com/emacs-lsp/lsp-ui/issues/243
+    (defadvice lsp-ui-imenu (after hide-lsp-ui-imenu-mode-line activate)
+      (setq mode-line-format nil))
+    ;; `C-g'to close doc
+    (advice-add #'keyboard-quit :before #'lsp-ui-doc-hide))
+;; (use-package dap-mode
+;;   :diminish
+;;   :bind
+;;   (:map dap-mode-map
+;;         (("<f12>" . dap-debug)
+;;          ("<f8>" . dap-continue)
+;;          ("<f9>" . dap-next)
+;;          ("<M-f11>" . dap-step-in)
+;;          ("C-M-<f11>" . dap-step-out)
+;;          ("<f7>" . dap-breakpoint-toggle))))
+
+;; Languages
+;; PHP
+(use-package php-mode :ensure t)
+(add-hook 'php-mode-hook 'php-enable-psr2-coding-style)
+(with-eval-after-load 'php-mode
+  (define-key php-mode-map (kbd "C-c C--") 'php-current-class)
+  (define-key php-mode-map (kbd "C-c C-=") 'php-current-namespace))
+
+;; Web-mode
+(use-package web-mode
+  :custom-face
+  (css-selector ((t (:inherit default :foreground "#66CCFF"))))
+  (font-lock-comment-face ((t (:foreground "#828282"))))
+  :mode
+  ("\\.phtml\\'" "\\.tpl\\'" ".php\\'" "\\.[agj]sp\\'" "\\.as[cp]x\\'"
+   "\\.erb\\'" "\\.mustache\\'" "\\.djhtml\\'" "\\.[t]?html?\\'"))
+
+;; LaTeX
+(use-package tex
+  :ensure auctex
+  :defer t
+  :custom
+  (TeX-auto-save t)
+  (TeX-parse-self t)
+  (TeX-master nil)
+  ;; to use pdfview with auctex
+  (TeX-view-program-selection '((output-pdf "pdf-tools"))
+                               TeX-source-correlate-start-server t)
+  (TeX-view-program-list '(("pdf-tools" "TeX-pdf-tools-sync-view")))
+  (TeX-after-compilation-finished-functions #'TeX-revert-document-buffer)
+  :hook
+  (LaTeX-mode . (lambda ()
+                  (turn-on-reftex)
+                  (setq reftex-plug-into-AUCTeX t)
+                  (reftex-isearch-minor-mode)
+                  (setq TeX-PDF-mode t)
+                  (setq TeX-source-correlate-method 'synctex)
+                  (setq TeX-source-correlate-start-server t)))
+  :config
+  (add-hook LaTeX-mode-hook 'display-line-numbers-mode))
 
 (provide 'init)
 ;;; init.el ends here
