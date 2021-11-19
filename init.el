4d12f0f91c0f82763a3a35bd39983a10a068fefd
Eugene Mah <eugenemah@gmail.com>
2021-11-19 11:34:14 -0500

    Add keybindings for org-time-stamp

--- init.el
+++ init.el
@@ -224,6 +224,8 @@
 (global-set-key (kbd "C-c l") #'org-store-link)
 (global-set-key (kbd "C-c a") #'org-agenda)
 (global-set-key (kbd "C-c c") #'org-capture)
+(global-set-key (kbd "C-c .") #'org-time-stamp)
+(global-set-key (kbd "C-c ,") #'org-time-stamp-inactive)
 
 ;; Org tags
 (setq org-tag-alist '(("WORK" . ?W)
