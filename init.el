77b9d67f87707923d6dbb2a50c24db951286e036
Eugene Mah <eugenemah@gmail.com>
2021-12-11 16:39:32 -0500

    Clean up init file.
    Try putting some of my personal keybindings into a minor mode

--- init.el
+++ init.el
@@ -82,6 +82,13 @@
 (load-theme 'tron-legacy t)
 (setq tron-legacy-theme-vivid-cursor t)
 
+;; Keybindings
+(global-set-key [mouse-6] 'backward-word)
+(global-set-key [mouse-7] 'forward-word)
+(global-set-key [mouse-8] 'scroll-up-command)
+(global-set-key [mouse-9] 'scroll-down-command)
+(global-set-key [mouse-10] 'list-buffers)
+
 ;; Package management setup
 (require 'package)
 (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
@@ -114,6 +121,266 @@
   (auto-package-update-maybe))
 (use-package diminish
   :ensure t)
+
+;; Org mode
+;; Use the version of org-mode that comes bundled with Fedora's emacs
+;; instead of grabbing it from ELPA.
+;; It will be a bit older, but then I don't need to worry about conflicts
+;; because of loading different versions
+(require 'org)
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+(add-hook 'org-mode-hook #'turn-on-font-lock)
+(setq org-agenda-files (list "~/org/todo.org")
+      org-default-notes-file "~/org/notes.org"
+      org-directory "~/org/"
+      org-enable-github-support t
+      org-enable-journal-support t
+      org-log-done 'time-date
+      org-startup-truncated nil
+      org-use-speed-commands t)
+;; Org tags
+(setq org-tag-alist '(("WORK" . ?W)
+                      ("home" . ?h)
+                      ("lab" . ?l)
+                      ("research" . ?r)
+                      ("dogs" . ?d)
+                      ("radioclub" . ?C)))
+;; Org journal
+(use-package org-journal
+  :defer t
+  :init
+  (setq org-journal-prefix-key "C-c j ")
+  :config
+  (setq org-journal-dir "~/org/journal/"
+        org-journal-enable-agenda-integration t))
+;; Org capture templates
+(defun org-journal-find-location ()
+  "Open today's journal, but specify a non-nil prefix argument in order to inhibit inserting the heading; 'org-capture' will insert the heading."
+  (org-journal-new-entry t)
+  (unless (eq org-journal-file-type 'daily)
+    (org-narrow-to-subtree))
+  (goto-char (point-max)))
+(setq org-capture-templates '(("t" "Todo"
+                               entry (file+headline "~/org/todo.org" "Tasks")
+                               "** TODO %?\n %i\n %a")
+                              ("a" "Appointment"
+                               entry (file+headline "~/org/todo.org" "Calendar")
+                               "** APPT %^{Description} %^g\n %?\n Added: %U")
+                              ("j" "Journal"
+                               entry (file+olp+datetree "~/org/journal/journal.org")
+                               "* %?\nEntered on %U\n %i\n %a")
+                              ("n" "Notes"
+                               entry (file+olp+datetree "~/org/notes.org")
+                               "* %^{Description} %^g %?\n Added: %U")
+                              ("l" "Lab book"
+                               entry (file+olp+datetree "~/org/PhD/notes.org")
+                               "* %U\n %?\n %i\n %a")
+                              ("j" "Journal entry" plain (function org-journal-find-location)
+                               "** %(format-time-string org-journal-time-format)%^{Title}\n%i%?"
+                               :jump-to-captured t :immediate-finish t)))
+
+;; Magit
+(use-package magit
+  :ensure t
+  :demand t
+  :bind
+  (("C-c C-g s" . magit-status)
+   ("C-c C-g d" . magit-diff)
+   ("C-c C-g S" . magit-stage)
+   ("C-c C-g u" . magit-unstage)
+   ("C-c C-g c" . magit-commit)
+   ("C-c C-g p" . magit-push)
+   ("C-c C-g P" . magit-pull)
+   ("C-c C-g f" . magit-fetch))
+  :config
+  (setq magit-credential-cache-daemon-socket nil)
+  (magit-auto-revert-mode))
+
+;; Company mode
+(use-package company
+  :diminish company-mode
+  :hook ((prog-mode LaTeX-mode latex-mode ess-r-mode) . company-mode)
+  :custom
+  (company-tooltip-align-annotations t)
+  (company-require-match 'never)
+  ;; Don't use company in the following modes
+  (company-global-modes '(not shell-mode eaf-mode))
+  (company-idle-delay 0.2)
+  (company-minimum-prefix-length 3)
+  (company-selection-wrap-around t)
+  (company-text-face-extra-attributes '(:weight bold))
+  :config
+  (global-company-mode 1))
+
+;; Helm
+(use-package helm
+  :demand t
+  :bind
+  (("M-x" . helm-M-x)
+   ("C-x C-f" . helm-find-files)
+   ("C-h i" . helm-info)
+   ("C-h a" . helm-apropos)
+   ("M-y" . helm-show-kill-ring)
+   ("C-x b" . helm-mini))
+  :config
+  (require 'helm-config)
+  (setq helm-input-idle-delay                     0.01
+        helm-always-two-windows                   t
+        helm-actions-inherit-frame-settings       t
+        helm-allow-mouse                          t
+        helm-autoresize-max-height                0 ; it is %.
+        helm-autoresize-min-height                10 ; it is %.
+        helm-autoresize-mode                      t
+        helm-buffers-fuzzy-matching               t
+        helm-candidate-number-limit               500
+        helm-commands-using-frame                 '(completion-at-point
+                                                    helm-apropos
+                                                    helm-eshell-prompts helm-imenu
+                                                    helm-imenu-in-all-buffers)
+        helm-display-buffer-default-height        10 ;; Make the helm buffer smaller
+        helm-follow-mode-persistent               t
+        helm-frame-background-color               "DarkSlateGray"
+        helm-move-to-line-cycle-in-source         t
+        helm-recentf-fuzzy-match                  t
+        helm-reuse-last-window-split-state        t
+        helm-show-action-window-other-window      'left
+        helm-split-window-inside-p                nil
+        helm-use-frame-when-more-than-two-windows t
+        helm-use-frame-when-no-suitable-window    t
+        helm-visible-mark-prefix                  "✓"))
+(use-package helm-org
+  :config
+  (setq helm-org-headings-fontify t))
+(helm-mode 1)
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+;;Smartparens
+(use-package smartparens
+  :diminish smartparens-mode
+  :bind
+  (:map smartparens-mode-map
+        ("C-)" . sp-forward-slurp-sexp)
+        ("C-}" . sp-forward-barf-sexp)
+        ("C-(" . sp-backward-slurp-sexp)
+        ("C-{" . sp-backward-barf-sexp))
+  :config
+  (progn
+    ;; Stop pairing single quotes in elisp
+    (sp-local-pair 'emacs-lisp-mode "'" nil :actions nil)
+    (sp-local-pair 'org-mode "[" nil :actions nil)
+    (sp-local-pair 'org-mode "~" "~")))
+;;Enable strict mode globally
+(smartparens-global-strict-mode 1)
+
+;; Which-key
+(use-package which-key
+  :ensure t
+  :init (which-key-mode)
+  :diminish which-key-mode)
+
+;; Languages
+;; PHP
+(use-package php-mode
+  :ensure t
+  :config
+  (setq php-mode-coding-style 'psr2))
+(use-package ac-php)
+(use-package company-php
+  :defer
+  :after (php-mode company))
+(add-to-list 'auto-mode-alist '("\\.php\\'" . php-mode))
+(setq php-mode-coding-style 'psr2)
+(add-hook 'php-mode-hook 'php-enable-psr2-coding-style)
+(add-hook 'php-mode-hook 'php-enable-default-coding-style)
+(add-hook 'php-mode-hook #'php-align-setup)
+(add-hook 'php-mode-hook
+          '(lambda ()
+             (company-mode t)
+             (require 'company-php)
+             (set (make-local-variable 'company-backends)
+                  '((company-ac-php-backend company-dabbrev-code)
+                    company-capf company-files))
+             (define-key php-mode-map (kbd "M-]")
+               'ac-php-find-symbol-at-point)
+             (define-key php-mode-map (kbd "M-[")
+               'ac-php-location-stack-back)))
+(with-eval-after-load 'php-mode
+  (define-key php-mode-map (kbd "C-c C--") 'php-current-class)
+  (define-key php-mode-map (kbd "C-c C-=") 'php-current-namespace))
+
+;; ;; Web-mode
+(use-package web-mode
+  :custom-face
+  (css-selector ((t (:inherit default :foreground "#66CCFF"))))
+  (font-lock-comment-face ((t (:foreground "#828282"))))
+  :mode
+  ("\\.phtml\\'" "\\.tpl\\.php\\'" "\\.[agj]sp\\'" "\\.as[cp]x\\'"
+   "\\.erb\\'" "\\.mustache\\'" "\\.djhtml\\'" "\\.[t]?html?\\'"
+   "\\.html?\\'" "\\.blade\\.php\\'")
+  :config
+  (setq web-mode-engines-alist
+        '(("php" . "\\.phtml\\'")
+          ("blade" . "\\.blade\\."))
+        web-mode-enable-auto-pairing t
+        web-mode-enable-auto-closing t
+        web-mode-enable-auto-indentation t
+        web-mode-enable-auto-opening t
+        web-mode-enable-auto-quoting t
+        web-mode-enable-css-colorization t
+        web-mode-enable-engine-detection t
+        web-mode-markup-indent-offset 2
+        web-mode-css-indent-offset 2
+        web-mode-code-indent-offset 2))
+
+;; LaTeX
+(require 'tex-site)
+(require 'reftex)
+(setq TeX-auto-save t)
+(setq TeX-parse-self t)
+(setq-default TeX-master nil)
+(setq TeX-PDF-mode t)
+(setq reftex-plug-into-AUCTeX t)
+(add-hook 'LaTeX-mode-hook 'auto-fill-mode)
+(add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
+(add-hook 'LaTeX-mode-hook 'turn-on-reftex)
+
+(use-package company-auctex
+  :defer t
+  :config
+  (company-auctex-init))
+
+(use-package company-math
+  :defer t)
+(with-eval-after-load 'company-math
+  (add-to-list 'company-backends 'company-math-symbols-latex)
+  (add-to-list 'company-backends 'company-math-symbols-unicode)
+  (add-to-list 'company-backends 'company-latex-commands))
+
+;; Define a minor mode to hold some of my keybindings
+(define-minor-mode em-keymaps-mode
+  "Personal keybindings"
+  :keymap (let ((map (make-sparse-keymap)))
+            ;; Emacs frame-related keybindings
+            (define-key map (kbd "C-c C-f d") 'delete-frame)
+            (define-key map (kbd "C-c C-f n") 'make-frame)
+            (define-key map (kbd "C-c C-f o") 'other-frame)
+            (define-key map (kbd "C-c C-f s") 'select-frame)
+            (define-key map (kbd "C-c C-f l") 'lower-frame)
+            (define-key map (kbd "C-c C-f r") 'raise-frame)
+            ;; Org-mode keybindings
+            (define-key map (kbd "C-c l") #'org-store-link)
+            (define-key map (kbd "C-c a") #'org-agenda)
+            (define-key map (kbd "C-c c") #'org-capture)
+            (define-key map (kbd "C-c .") #'org-time-stamp)
+            (define-key map (kbd "C-c ,") #'org-time-stamp-inactive)
+            ;; Other keybindings
+            (define-key map (kbd "C-c ;") 'comment-or-uncomment-region)
+            (define-key map (kbd "<escape>") 'keyboard-escape-quit)
+            map))
+(add-hook 'text-mode-hook 'em-keymaps-mode)
+(em-keymaps-mode 1)
 
 (provide 'init)
 ;;; init.el ends here
