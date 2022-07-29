b8b3abf802acdfb2b5eef7353b1878b5a1a7afa9
Eugene Mah <eugenemah@gmail.com>
2022-07-29 19:39:40 -0400

    Move LIBRARY_PATH definition to config.fish

--- early-init.el
+++ early-init.el
@@ -5,10 +5,6 @@
 (setq package-enable-at-startup nil)
 ;; Don't ask if emacs should follow symlinks
 (setq vc-follow-symlinks t)
-(setenv "LIBRARY_PATH"
-  (string-join
-   '("/usr/lib64"
-     "/usr/lib/gcc/x86_64-redhat-linux/12/") ":"))
 
 (provide 'early-init)
 ;;; early-init ends here
