1286a229e059a69e08ba1c999bbeef739ba1b26d
Eugene Mah <eugenemah@gmail.com>
2022-09-12 08:38:08 -0400

    Remove LIBRARY_PATH definition

--- early-init.el
+++ early-init.el
@@ -1,3 +1,4 @@
+
 ;;; early-init.el --- -*- lexical-binding:t -*-
 
 ;;; Code:
@@ -5,10 +6,6 @@
 (setq package-enable-at-startup nil)
 ;; Don't ask if emacs should follow symlinks
 (setq vc-follow-symlinks t)
-(setenv "LIBRARY_PATH"
-  (string-join
-   '("/usr/lib64"
-     "/usr/lib/gcc/x86_64-redhat-linux/12/") ":"))
 
 (provide 'early-init)
 ;;; early-init ends here
