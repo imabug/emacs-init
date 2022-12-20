773b95825c9076a236cb5bd6b67d6aeaf18bd3b3
Eugene Mah <eugenemah@gmail.com>
2022-12-19 21:21:59 -0500

    Rework init.el to use local packages and straight.el

--- init.el
+++ init.el
@@ -38,6 +38,7 @@
               use-short-answers t)
 (prefer-coding-system 'utf-8)
 (global-display-line-numbers-mode t)       ;Display line numbers
+(global-auto-revert-mode t)
 ;; Disable line numbers for some modes
 (dolist (mode '(org-mode-hook
                 term-mode-hook
@@ -94,29 +95,35 @@
 (global-set-key [mouse-9] 'scroll-down-command)
 (global-set-key [mouse-10] 'list-buffers)
 
-;; Package management setup
-(require 'package)
-(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
-                         ("melpa" . "https://melpa.org/packages/")
-                         ("nongnu" . "https://elpa.nongnu.org/nongnu/")))
-(package-initialize)
-(unless package-archive-contents
-  (package-refresh-contents))
+;; Bootstrap code for straight.el
+(setq straight-use-package-by-default t)
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
-;; Install use-package if it's not already installed
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-(require 'use-package)
+;; Install use-package
+(straight-use-package 'use-package)
 (setq use-package-verbose t
+      load-prefer-newer t
       use-package-always-ensure t
-      load-prefer-newer t)
+      use-package-always-defer t)
 (use-package auto-compile
+  :straight t
   :config
   (auto-compile-on-load-mode)
   (auto-compile-on-save-mode))
 (use-package auto-package-update
-  :if (not (daemonp))
+  :straight t
+  :if (not(daemonp))
   :custom
   (auto-package-update-interval 7)
   (auto-package-update-prompt-before-update t)
@@ -125,16 +132,210 @@
   :config
   (auto-package-update-maybe))
 (use-package diminish
-  :ensure t)
+  :straight t)
+
+;; Rainbow-delimiters
+(use-package rainbow-delimiters
+  :straight t)
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+(rainbow-delimiters-mode t)
+
+;;Smartparens
+(use-package smartparens
+  :straight t
+  :diminish smartparens-mode
+  :bind
+  (:map smartparens-mode-map
+        ("C-)" . sp-forward-slurp-sexp)
+        ("C-}" . sp-forward-barf-sexp)
+        ("C-(" . sp-backward-slurp-sexp)
+        ("C-{" . sp-backward-barf-sexp))
+  :config
+  (progn
+    ;; Stop pairing single quotes in elisp
+    (sp-local-pair 'emacs-lisp-mode "'" nil :actions nil)
+    (sp-local-pair 'org-mode "[" nil :actions nil)
+    (sp-local-pair 'org-mode "~" "~")))
+;;; Some config borrowed from https://github.com/Fuco1/.emacs.d/blob/master/files/smartparens.el
+;;; lisp modes
+(require 'smartparens-config)
+(sp-with-modes sp--lisp-modes
+  (sp-local-pair "(" nil
+                 :wrap "C-("
+                 :pre-handlers '(my-add-space-before-sexp-insertion)
+                 :post-handlers '(my-add-space-after-sexp-insertion)))
+
+(defun my-add-space-after-sexp-insertion (id action _context)
+  (when (eq action 'insert)
+    (save-excursion
+      (forward-char (sp-get-pair id :cl-l))
+      (when (or (eq (char-syntax (following-char)) ?w)
+                (looking-at (sp--get-opening-regexp)))
+        (insert " ")))))
+
+(defun my-add-space-before-sexp-insertion (id action _context)
+  (when (eq action 'insert)
+    (save-excursion
+      (backward-char (length id))
+      (when (or (eq (char-syntax (preceding-char)) ?w)
+                (and (looking-back (sp--get-closing-regexp))
+                     (not (eq (char-syntax (preceding-char)) ?'))))
+        (insert " ")))))
+
+;;; Smartparens PHP
+(sp-with-modes '(php-mode)
+  (sp-local-pair "/**" "*/" :post-handlers '(("| " "SPC")
+                                             (my-php-handle-docstring "RET")))
+  (sp-local-pair "/*." ".*/" :post-handlers '(("| " "SPC")))
+  (sp-local-pair "{" nil :post-handlers '(("||\n[i]" "RET") my-php-wrap-handler))
+  (sp-local-pair "(" nil :prefix "\\(\\sw\\|\\s_\\)*"))
+
+(defun my-php-wrap-handler (&rest _ignored)
+  (save-excursion
+    (sp-get sp-last-wrapped-region
+      (goto-char :beg-in)
+      (unless (looking-at "[ \t]*$")
+        (newline-and-indent))
+      (goto-char :end-in)
+      (beginning-of-line)
+      (unless (looking-at "[ \t]*}[ \t]*$")
+        (goto-char :end-in)
+        (newline-and-indent))
+      (indent-region :beg-prf :end-suf))))
+
+(defun my-php-handle-docstring (&rest _ignored)
+  (-when-let (line (save-excursion
+                     (forward-line)
+                     (thing-at-point 'line)))
+    (cond
+     ;; variable
+     ((string-match (rx (or "private" "protected" "public" "var") (1+ " ") (group "$" (1+ alnum))) line)
+      (let ((var-name (match-string 1 line))
+            (type ""))
+        ;; try to guess the type from the constructor
+        (-when-let (constructor-args (my-php-get-function-args "__construct" t))
+          (setq type (or (cdr (assoc var-name constructor-args)) "")))
+        (insert "* @var " type)
+        (save-excursion
+          (insert "\n"))))
+     ((string-match-p "function" line)
+      (save-excursion
+        (let ((args (save-excursion
+                      (forward-line)
+                      (my-php-get-function-args nil t))))
+          (--each args
+            (when (my-php-should-insert-type-annotation (cdr it))
+              (insert (format "* @param %s%s\n"
+                              (my-php-translate-type-annotation (cdr it))
+                              (car it))))))
+        (let ((return-type (save-excursion
+                             (forward-line)
+                             (my-php-get-function-return-type))))
+          (when (my-php-should-insert-type-annotation return-type)
+            (insert (format "* @return %s\n" (my-php-translate-type-annotation return-type))))))
+      (re-search-forward (rx "@" (or "param" "return") " ") nil t))
+     ((string-match-p ".*class\\|interface" line)
+      (save-excursion (insert "\n"))
+      (insert "* ")))
+    (let ((o (sp--get-active-overlay)))
+      (indent-region (overlay-start o) (overlay-end o)))))
+;;Enable strict mode globally
+(smartparens-global-strict-mode 1)
+
+;; Which-key
+(use-package which-key
+  :init
+  (which-key-setup-side-window-right-bottom)
+  (which-key-mode))
+
+;; Projectile
+(use-package projectile
+  :straight t
+  :bind (:map projectile-mode-map
+              ("C-c p" . projectile-command-map))
+  :init
+  (setq projectile-mode-line-function '(lambda () (format "[%s]" (projectile-project-name)))
+        projectile-sort-order 'recentf
+        projectile-enable-caching t)
+  :config
+  (projectile-mode +1))
+
+(require 'magit)
+(setq magit-credential-cache-daemon-socket nil
+      magit-refresh-status-buffer nil
+      magit-auto-revert-mode t
+      magit-define-global-key-bindings t)
+
+(straight-use-package '(company :type built-in))
+(setq company-backends '(company-capf
+                         company-dabbrev-code
+                         company-keywords)
+      company-frontends '(company-pseudo-tooltip-unless-just-one-frontend-with-delay
+                          company-preview-frontend
+                          company-echo-metadata-frontend)
+      company-format-margin-function 'company-vscode-dark-icons-margin
+      company-global-modes '(not shell-mode eaf-mode)
+      company-idle-delay 0.2
+      company-minimum-prefix-length 3
+      company-show-numbers t
+      company-tooltip-align-annotations t
+      company-require-match nil
+      company-selection-wrap-around t)
+;;(add-hook 'LaTeX-mode #'company-mode)
+(add-hook 'after-init-hook 'global-company-mode)
+
+;; Helm
+(use-package helm
+  :straight t
+  :demand t
+  :bind
+  (("M-x" . helm-M-x)
+   ("C-x C-f" . helm-find-files)
+   ("C-h i" . helm-info)
+   ("C-h a" . helm-apropos)
+   ("M-y" . helm-show-kill-ring)
+   ("C-x b" . helm-mini))
+  :config
+  (setq helm-actions-inherit-frame-settings       t
+        helm-always-two-windows                   t
+        helm-allow-mouse                          t
+        helm-autoresize-max-height                0 ; it is %.
+        helm-autoresize-min-height                10 ; it is %.
+        helm-autoresize-mode                      t
+        helm-buffers-fuzzy-matching               t
+        helm-candidate-number-limit               500
+        helm-commands-using-frame                 '(completion-at-point
+                                                    helm-apropos
+                                                    helm-eshell-prompts helm-imenu
+                                                    helm-imenu-in-all-buffers)
+        helm-display-buffer-default-height        10 ;; Make the helm buffer smaller
+        helm-follow-mode-persistent               t
+        helm-frame-background-color               "DarkSlateGray"
+        helm-input-idle-delay                     0.01
+        helm-move-to-line-cycle-in-source         t
+        helm-recentf-fuzzy-match                  t
+        helm-reuse-last-window-split-state        t
+        helm-show-action-window-other-window      'left
+        helm-split-window-inside-p                nil
+        helm-use-frame-when-more-than-two-windows t
+        helm-use-frame-when-no-suitable-window    t
+        helm-visible-mark-prefix                  "✓"))
+(use-package helm-org
+  :straight t
+  :demand t
+  :config
+  (setq helm-org-headings-fontify t))
+(use-package helm-projectile
+  :straight t
+  :demand t
+  :after projectile)
+(helm-mode 1)
+(helm-projectile-on)
+(setq projectile-completion-system 'helm
+      projectile-switch-project-action 'helm-projectile)
 
 ;; Org mode
-;; Use the version of org-mode that comes bundled with Fedora's emacs
-;; instead of grabbing it from ELPA.
-;; It will be a bit older, but then I don't need to worry about conflicts
-;; because of loading different versions
-(require 'org)
-(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
-(add-hook 'org-mode-hook #'turn-on-font-lock)
+(straight-use-package '(org :type built-in))
 (setq org-directory "~/org/"
       org-agenda-files (list "~/org/todo.org")
       org-default-notes-file "~/org/notes.org"
@@ -144,32 +345,14 @@
       org-log-done 'time-date
       org-startup-truncated nil
       org-use-speed-commands t
-      org-return-follows-link t)
-;; Org tags
-(setq org-tag-alist '(("WORK" . ?W)
+      org-return-follows-link t
+      org-tag-alist '(("WORK" . ?W)
                       ("home" . ?h)
                       ("lab" . ?l)
                       ("research" . ?r)
                       ("dogs" . ?d)
-                      ("radioclub" . ?C)))
-;; Org journal
-(use-package org-journal
-  :ensure t
-  :defer t
-  :init
-  (setq org-journal-prefix-key "C-c j ")
-  :config
-  (setq org-journal-dir "~/org/journal/"
-        org-journal-file-type 'weekly
-        org-journal-enable-agenda-integration t))
-;; Org capture templates
-(defun org-journal-find-location ()
-  "Open today's journal, but specify a non-nil prefix argument in order to inhibit inserting the heading; 'org-capture' will insert the heading."
-  (org-journal-new-entry t)
-  (unless (eq org-journal-file-type 'daily)
-    (org-narrow-to-subtree))
-  (goto-char (point-max)))
-(setq org-capture-templates '(("t" "Todo"
+                      ("radioclub" . ?C))
+      org-capture-templates '(("t" "Todo"
                                entry (file+headline "todo.org" "Tasks")
                                "** TODO %?\n %i\n %a")
                               ("a" "Appointment"
@@ -188,140 +371,35 @@
                               ("l" "Lab book"
                                entry (file+olp+datetree "PhD/notes.org")
                                "* %U\n %?\n %i\n %a")))
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+(add-hook 'org-mode-hook #'turn-on-font-lock)
 
-;; Projectile
-(use-package projectile
-  :ensure t
-  :bind (:map projectile-mode-map
-              ("C-c p" . projectile-command-map))
-  :init
-  (setq projectile-mode-line-function '(lambda () (format "[%s]" (projectile-project-name))))
-  :config
-  (projectile-mode +1)
-)
-
-;; Magit
-(use-package magit
-  :ensure t
+;; Org journal
+(use-package org-journal
   :demand t
-  :bind
-  (("C-c C-g s" . magit-status)
-   ("C-c C-g d" . magit-diff)
-   ("C-c C-g S" . magit-stage)
-   ("C-c C-g u" . magit-unstage)
-   ("C-c C-g c" . magit-commit)
-   ("C-c C-g p" . magit-push)
-   ("C-c C-g P" . magit-pull)
-   ("C-c C-g f" . magit-fetch))
-  :config
-  (setq magit-credential-cache-daemon-socket nil)
-  (magit-auto-revert-mode))
-
-;; Company mode
-(use-package company
-  :diminish company-mode
-  :hook ((prog-mode LaTeX-mode latex-mode ess-r-mode) . company-mode)
   :init
-  (setq company-idle-delay 0.2
-        company-minimum-prefix-length 3
-        company-tooltip-align-annotations t
-        company-require-match nil
-        company-selection-wrap-around t)
-  :custom
-  ;; Don't use company in the following modes
-  (company-global-modes '(not shell-mode eaf-mode))
-  (company-text-face-extra-attributes '(:weight bold))
+  (setq org-journal-prefix-key "C-c j ")
   :config
-  (global-company-mode 1))
-
-;; Helm
-(use-package helm
-  :demand t
-  :bind
-  (("M-x" . helm-M-x)
-   ("C-x C-f" . helm-find-files)
-   ("C-h i" . helm-info)
-   ("C-h a" . helm-apropos)
-   ("M-y" . helm-show-kill-ring)
-   ("C-x b" . helm-mini))
-  :config
-  (require 'helm-config)
-  (setq helm-input-idle-delay                     0.01
-        helm-always-two-windows                   t
-        helm-actions-inherit-frame-settings       t
-        helm-allow-mouse                          t
-        helm-autoresize-max-height                0 ; it is %.
-        helm-autoresize-min-height                10 ; it is %.
-        helm-autoresize-mode                      t
-        helm-buffers-fuzzy-matching               t
-        helm-candidate-number-limit               500
-        helm-commands-using-frame                 '(completion-at-point
-                                                    helm-apropos
-                                                    helm-eshell-prompts helm-imenu
-                                                    helm-imenu-in-all-buffers)
-        helm-display-buffer-default-height        10 ;; Make the helm buffer smaller
-        helm-follow-mode-persistent               t
-        helm-frame-background-color               "DarkSlateGray"
-        helm-move-to-line-cycle-in-source         t
-        helm-recentf-fuzzy-match                  t
-        helm-reuse-last-window-split-state        t
-        helm-show-action-window-other-window      'left
-        helm-split-window-inside-p                nil
-        helm-use-frame-when-more-than-two-windows t
-        helm-use-frame-when-no-suitable-window    t
-        helm-visible-mark-prefix                  "✓"))
-(use-package helm-org
-  :config
-  (setq helm-org-headings-fontify t))
-(use-package helm-projectile)
-(helm-mode 1)
-(helm-projectile-on)
-(setq projectile-completion-system 'helm
-      projectile-switch-project-action 'helm-projectile)
-
-(use-package rainbow-delimiters
-  :ensure t)
-
-(use-package prog-mode
-  :ensure nil
-  :hook ((prog-mode . rainbow-delimiters-mode)))
-
-;;Smartparens
-(use-package smartparens
-  :diminish smartparens-mode
-  :bind
-  (:map smartparens-mode-map
-        ("C-)" . sp-forward-slurp-sexp)
-        ("C-}" . sp-forward-barf-sexp)
-        ("C-(" . sp-backward-slurp-sexp)
-        ("C-{" . sp-backward-barf-sexp))
-  :config
-  (progn
-    ;; Stop pairing single quotes in elisp
-    (sp-local-pair 'emacs-lisp-mode "'" nil :actions nil)
-    (sp-local-pair 'org-mode "[" nil :actions nil)
-    (sp-local-pair 'org-mode "~" "~")))
-;;Enable strict mode globally
-(smartparens-global-strict-mode 1)
-
-;; Which-key
-(use-package which-key
-  :ensure t
-  :init (which-key-mode)
-  :diminish which-key-mode)
+  (setq org-journal-dir "~/org/journal/"
+        org-journal-file-type 'weekly
+        org-journal-enable-agenda-integration t))
+;; Org capture templates
+(defun org-journal-find-location ()
+  "Open today's journal, but specify a non-nil prefix argument in order to inhibit inserting the heading; 'org-capture' will insert the heading."
+  (org-journal-new-entry t)
+  (unless (eq org-journal-file-type 'daily)
+    (org-narrow-to-subtree))
+  (goto-char (point-max)))
 
 ;; Languages
 ;; PHP
 (use-package php-mode
-  :ensure t
   :config
   (setq php-mode-coding-style 'psr2))
 (use-package ac-php)
 (use-package company-php
-  :defer
   :after (php-mode company))
 (add-to-list 'auto-mode-alist '("\\.php\\'" . php-mode))
-(setq php-mode-coding-style 'psr2)
 (add-hook 'php-mode-hook 'php-enable-psr2-coding-style)
 (add-hook 'php-mode-hook 'php-enable-default-coding-style)
 (add-hook 'php-mode-hook #'php-align-setup)
@@ -365,27 +443,33 @@
         web-mode-code-indent-offset 2))
 
 ;; LaTeX
-;;(require 'tex-site)
-(use-package tex
-  :ensure auctex
+(require 'tex-site)
+(setq TeX-auto-save t
+      TeX-parse-self t
+      TeX-PDF-mode t
+      TeX-master nil)
+;; (use-package tex
+;;   :ensure auctex
+;;   :config
+;;   (setq TeX-auto-save t
+;;         TeX-parse-self t
+;;         TeX-PDF-mode t
+;;         TeX-master nil))
+(use-package reftex
+  :straight t
   :config
-  (setq TeX-auto-save t
-        TeX-parse-self t
-        TeX-PDF-mode t
-        TeX-master nil))
-(use-package reftex)
-(setq reftex-plug-into-AUCTeX t)
+  (setq reftex-plug-into-AUCTeX t))
 (add-hook 'LaTeX-mode-hook 'auto-fill-mode)
 (add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
 (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
 
 (use-package company-auctex
-  :defer t
+  :straight t
   :config
   (company-auctex-init))
 
 (use-package company-math
-  :defer t)
+  :straight t)
 (with-eval-after-load 'company-math
   (add-to-list 'company-backends 'company-math-symbols-latex)
   (add-to-list 'company-backends 'company-math-symbols-unicode)
@@ -409,6 +493,15 @@
             (define-key map (kbd "C-c c") #'org-capture)
             (define-key map (kbd "C-c .") #'org-time-stamp)
             (define-key map (kbd "C-c ,") #'org-time-stamp-inactive)
+            ;; Magit keybindings
+            (define-key map (kbd "C-c C-g s") 'magit-status)
+            (define-key map (kbd "C-c C-g d") 'magit-diff)
+            (define-key map (kbd "C-c C-g S") 'magit-stage)
+            (define-key map (kbd "C-c C-g u") 'magit-unstage)
+            (define-key map (kbd "C-c C-g c") 'magit-commit)
+            (define-key map (kbd "C-c C-g p") 'magit-push)
+            (define-key map (kbd "C-c C-g P") 'magit-pull)
+            (define-key map (kbd "C-c C-g f") 'magit-fetch)            
             ;; Other keybindings
             (define-key map (kbd "C-c ;") 'comment-or-uncomment-region)
             (define-key map (kbd "<escape>") 'keyboard-escape-quit)
@@ -433,7 +526,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(css-selector ((t (:inherit default :foreground "#66CCFF"))))
+ '(css-selector ((t (:inherit default :foreground "#66CCFF"))) t)
  '(font-lock-comment-face ((t (:foreground "#828282"))))
  '(org-agenda-structure ((t (:background "#1B324B" :foreground "#BBF0EF" :box nil :weight normal))))
  '(org-done ((t (:foreground "#BBF0EF" :box nil :weight bold))))
