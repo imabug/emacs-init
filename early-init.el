f3dea5379e500a48d238471a9a0a85fb7daa4513
Eugene Mah <eugenemah@gmail.com>
2025-01-04 11:33:34 -0500

    Remove duplicated line
    Remove duplicated line and go back to default gc threshold

--- early-init.el
+++ early-init.el
@@ -1,14 +1,11 @@
 ;;; early-init.el --- -*- lexical-binding:t -*-
 
 ;;; Code:
-(setq gc-cons-threshold 100000000)
+;; Don't enable package.el
 (setq package-enable-at-startup nil)
 
 ;; Don't ask if emacs should follow symlinks
 (setq vc-follow-symlinks t)
-
-;; Don't enable package.el
-(setq package-enable-at-startup nil)
 
 ;; Path settings
 (add-to-list 'load-path "~/.config/emacs/elisp")
